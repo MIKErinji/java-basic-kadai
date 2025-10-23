31108bde8b06583b9f4a51720ad34fb088a3dfcd
MIKErinji <hikawamaru12266@gmail.com>
2025-10-23 22:24:31 +0900

    2kaime

--- src/kadai_015/CarExec_Chapter15.java
+++ src/kadai_015/CarExec_Chapter15.java
@@ -4,6 +4,12 @@ public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		Car_Chapter15 car = new Car_Chapter15(1,10);
+		
+		car.changeGear(3);
+		
+		car.run();
 
 	}
 
