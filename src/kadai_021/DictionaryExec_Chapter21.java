e1922a27b7e0aca2b34e5e688c14052b7c2e222e
MIKErinji <hikawamaru12266@gmail.com>
2025-10-31 12:53:24 +0900

    1kai

---
+++ src/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,15 @@
+package kadai_021;
+
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 list = new Dictionary_Chapter21();
+		
+		String[] word = {"apple", "banana", "grape", "orange"};
+		
+		list.kensaku(word);
+	}
+
+}
---
+++ src/kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,39 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+
+public class Dictionary_Chapter21 {
+	
+	public void kensaku(String[] arr) {
+		
+	HashMap<String,String> dictionary = new HashMap<String,String>();
+	
+	dictionary.put("apple","りんご");
+	dictionary.put("peach","桃");
+	dictionary.put("banana","バナナ");
+	dictionary.put("lemon","レモン");
+	dictionary.put("pear","梨");
+	dictionary.put("kiwi","キウィ");
+	dictionary.put("strawberry","いちご");
+	dictionary.put("grape","ぶどう");
+	dictionary.put("muscat","マスカット");
+	dictionary.put("cherry","さくらんぼ");
+	
+	
+		 
+	for(String searchValue : arr) {
+		if( dictionary.get(searchValue) == null ) {
+			System.out.println(searchValue + "は辞書に存在しません");
+		} else {
+			System.out.println(searchValue + "の意味は" + dictionary.get(searchValue));
+		}
+		
+	}
+	 }
+}
+
+	
+	
+	
+
